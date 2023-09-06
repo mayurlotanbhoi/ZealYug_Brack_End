3e8824f4147b997a73ba4b049a2712e4f9af3c61
mayur-gamesapp <mayurbhoi200@gmail.com>
2023-09-06 20:03:55 +0530

    add url

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 app.use(
         cors({
-                origin: ["*", 'http://localhost:3001', "http://localhost:3001"],
+                origin: ["*", 'http://localhost:3000', "http://localhost:3001"],
         })
 );
 app.use(Express.json());
